a307af837eb03e6764a1b64b26ea500f5ded885a
Tufail Shoukat <154624057+TufailShoukat@users.noreply.github.com>
2024-03-05 11:58:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+//Chapter-1 (All Questions)
+
+//===== Chapter-1 (Q-1) ====
+// alert("Most Welcome!");
+
+//===== Chapter-1 (Q-2) ====
+// alert("Error! Plz Enter the valid password.")
+
+//===== Chapter-1 (Q-3) ====
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+//===== Chapter-1 (Q-4) ====
+// alert("Welcome to JS Land!");
+
+//===== Chapter-1 (Q-5) ====
+// alert("Prevent this page from crating additional dailogs.");
+
+//===== Chapter-1 (Q-6) ====
+// console.log("Hello..I can run JS  through my web browser's console");
+
+
+//Chapter-2 (All Questions)
+
+//===== Chapter-2 (Q-1) ====
+// var userName = "Tufail";
+// var myName   = " Ahmed";
+// var  fullName = userName + myName
+// alert(fullName)
+
+//===== Chapter-2 (Q-2) ====
+// var message  = "Hello World";
+// alert(message);  
+ 
+//===== Chapter-2 (Q-3) ====
+// var age = "17 years old";
+// alert(age);
+
+//===== Chapter-2 (Q-4) ====
+// var data = "Certified Mobile Application Development";
+// alert(data)
+
+//===== Chapter-2 (Q-5) ====
+// var thing  =  "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(thing);
+
+
+//===== Chapter-2 (Q-6) ====
+// var email = "tufailshoukat72@gmail.com";
+// alert(email);
+
+
+//===== Chapter-2 (Q-7) ====
+// var book = " I am trying to learn from the Book A smarter \n way to learn JavaScript";
+// alert(book);
+
+
+//===== Chapter-2 (Q-8) ====
+// var p  ="Yah! I can write HTML Content through Java Script";
+// document.write(p);
+
+
+//===== Chapter-2 (Q-9) ====
+// var des = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(des);
+
+
+// //===== Chapter-2 (Q-10) ====
+// var des1 =  "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// document.write(des1);
+
+
+//Chapter-3 (All Questions)
+
+
+// //===== Chapter-3 (Q-1) ====
+// var age = " Iam 17 years old";
+// alert(age)
+
+
+// //===== Chapter-3 (Q-2) ====
+
+// var visit = " He has visited this site 14 times";
+// alert(visit);
+
+
+// // //===== Chapter-3 (Q-3) ====
+// var birthDay = "My birth year is 2006";
+// document.write(birthDay);
+
+
+// // //===== Chapter-3  (Q-4) ====
+// document.write(birthDay);
+
+
+// // //===== Chapter-3  (Q-5) ====
+// var visitor = " <b> Tufail Ahmed </b> ordered <b> 7-T-shirt </b> (s) on spuer clothing store";
+// document.write(visitor);
+
+
+//Chapter-4 (All Questions)
+
+
+// // // //===== Chapter-4  (Q-1) ====
+//  var fullName = "Tufail";
+//  var lastName = "Shoukat";
+//  var surName  = "Katto";
+//  var fullData  = fullName + lastName + surName;
+//  alert(fullData);
+
+ 
+// // //===== Chapter-4  (Q-2) ====
+
+
+ //(i)Legal Names;
+
+ //  var = fullName;
+ //  var = $userName;
+ //  var = last_Name;
+ //  var = num1;
+ //  var =  full$000;
+
+ //(ii)ilLegal Names;
+
+  //  var = Fullname;
+  // var = 9num;
+ //  var = &name;
+  // var = Last name;
+//  var = @@&name;
+
+
+ 
+// // //===== Chapter-4  (Q-3) ====
+
+// var heading = "Rules for naming JS variables";
+// document.write("<h1>" + heading + "</h1>");
+
+// var line1 = "Variables names can only contain , numbers, $ and _ . For example : $my_1stVariable";
+// var line2 = "Variable must begin with a letter , $ or _ . For example : $name, _name or name";
+// var line3 = "Variable names are case sensitive";
+// var line4 = "Variable names should not be JS Keywords";
+// var alllines = line1 + "<br>" + line2 + "<br>" + line3 + "<br>" + line4;
+// document.write(alllines);
+
+
+
+
+
+
+
+ 
+
